7b2788d8dd7de42778ce6819ba9066c392841631
MarkusTech <wm.recopelacion@gmail.com>
2025-01-06 11:46:47 +0800

    Update Customers.jsx

--- frontend/src/admin/components/Customers.jsx
+++ frontend/src/admin/components/Customers.jsx
@@ -1,45 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Table from "./Table";
-import customers from "../../data/customers.json";
-const customerTableHead = [
-  "",
-  "name",
-  "email",
-  "phone",
-  "total orders",
-  "total spend",
-  "location",
-];
+import axios from "axios";
+
+const customerTableHead = ["ID", "Name", "Phone", "Email", "Address", "Type"];
 
 const renderHead = (item, index) => <th key={index}>{item}</th>;
 
 const renderBody = (item, index) => (
   <tr key={index}>
-    <td>{item.id}</td>
-    <td>{item.name}</td>
+    <td>{item.customerID}</td>
+    <td>
+      {`${item.firstName} ${item.middleName ? item.middleName + " " : ""}${
+        item.lastName
+      }`}
+    </td>
+    <td>{item.contactNo}</td>
     <td>{item.email}</td>
-    <td>{item.phone}</td>
-    <td>{item.total_orders}</td>
-    <td>{item.total_spend}</td>
-    <td>{item.location}</td>
+    <td>{item.address}</td>
+    <td>{item.customerType?.TypeName || "N/A"}</td>
   </tr>
 );
 
 const Customers = () => {
+  const [customers, setCustomers] = useState([]);
+
+  const loadCustomers = async () => {
+    try {
+      const response = await axios.get("/api/v2/customers");
+      if (response.data && response.data.data) {
+        setCustomers(response.data.data);
+      }
+    } catch (error) {
+      console.error("Error fetching customers:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadCustomers();
+  }, []);
+
   return (
     <div className="customers">
-      <h3 className="page-header">customers</h3>
+      <h3 className="page-header">Customers</h3>
       <div className="row">
         <div className="col-12">
           <div className="card">
             <div className="card__body">
-              <Table
-                limit="10"
-                headData={customerTableHead}
-                renderHead={(item, index) => renderHead(item, index)}
-                bodyData={customers}
-                renderBody={(item, index) => renderBody(item, index)}
-              />
+              {customers.length > 0 ? (
+                <Table
+                  limit="10"
+                  headData={customerTableHead}
+                  renderHead={renderHead}
+                  bodyData={customers}
+                  renderBody={renderBody}
+                />
+              ) : (
+                <p>No customer data available.</p>
+              )}
             </div>
           </div>
         </div>
