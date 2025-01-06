0ffd09411294f19dc7f0f0465c9b8276cae159c4
MarkusTech <wm.recopelacion@gmail.com>
2025-01-06 15:51:43 +0800

    update

--- backend/src/routes/purchaseOrderButton.routes.ts
+++ backend/src/routes/purchaseOrderButton.routes.ts
@@ -4,9 +4,16 @@ import { PurchaseOrderApprovalController } from "./../controller/purchaseOrderAp
 const router = Router();
 const purchaseOrderApprovalController = new PurchaseOrderApprovalController();
 
+// Route to cancel a purchase order
 router.put(
   "/purchase-orders/cancel/:poID",
   purchaseOrderApprovalController.cancelPurchaseOrder
+);
+
+// Route to approve a purchase order
+router.put(
+  "/purchase-orders/approve/:poID",
+  purchaseOrderApprovalController.approvePurchaseOrder
 );
 
 export default router;
